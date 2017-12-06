788e6e5f47b57ff58c12af0cbf43312632eaff05
ethancrist <ethancrist96@gmail.com>
2017-12-06 01:03:46 +0000

    Fixed alias destination

--- init.sh
+++ init.sh
@@ -4,6 +4,6 @@ echo "Installing cd-shortcut...";
 touch ~/.keys.cds
 cp cd-shortcut/cds.sh /usr/local/bin/cds;
 rm -R cd-shortcut;
-echo "alias cds='source /usr/local/bin/cds'" >> ~/.bash_profile
+echo "alias cds='source /usr/local/bin/cds'" >> ~/.bashrc
 alias cds="source /usr/local/bin/cds"
 echo "cd-shortcut installed. Type 'cds' for help"
