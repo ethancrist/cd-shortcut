2c60f11d9c42fffb5675f399b5b63fc145d44ee8
ethancrist <ethancrist96@gmail.com>
2017-12-06 00:52:36 +0000

    Command now accessible immediately after install

--- init.sh
+++ init.sh
@@ -5,4 +5,5 @@ touch ~/.keys.cds
 cp cd-shortcut/cds.sh /usr/local/bin/cds;
 rm -R cd-shortcut;
 echo "alias cds='source /usr/local/bin/cds'" >> ~/.bash_profile
+alias cds="source /usr/local/bin/cds"
 echo "cd-shortcut installed. Type 'cds' for help"
